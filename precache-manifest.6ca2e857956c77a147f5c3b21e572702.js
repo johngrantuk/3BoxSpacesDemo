c6e162027af313ee3f862c15e110bcff94225b2c
John G Mac2 <johngrantuk@googlemail.com>
2019-08-07 21:02:43 +0100

    Updates

---
+++ precache-manifest.6ca2e857956c77a147f5c3b21e572702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8955ba3df94f7df979d80b5f083b8712",
+    "url": "/3boxpizza/index.html"
+  },
+  {
+    "revision": "771c82d0083aee56315b",
+    "url": "/3boxpizza/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/3boxpizza/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "771c82d0083aee56315b",
+    "url": "/3boxpizza/static/js/main.2997d17e.chunk.js"
+  },
+  {
+    "revision": "da6550d6ff48bad6b72b",
+    "url": "/3boxpizza/static/js/runtime~main.f822c543.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/3boxpizza/static/media/logo.5d5d9eef.svg"
+  }
+]);
