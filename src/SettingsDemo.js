8d27e893a9b0db09c0849e0c96e69890f8dc4e81
John G Mac2 <johngrantuk@googlemail.com>
2019-08-09 16:42:42 +0100

    Tidided code and commented.

--- src/SettingsDemo.js
+++ src/SettingsDemo.js
@@ -7,12 +7,12 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      heading: 'What If I Told You',
-      backgroundColor: '',
+      heading: '3Box Spaces #BuildBetter',
+      backgroundColor: '',                                                      // Default values to display when no 3Box user settings loaded
       fontSize:'20px',
-      text: 'You Could Control Your Own Data??',
+      text: 'Do You Could Control Your Own Data??',
       fontFamily: 'courier',
-      ethAddress: '0xf8b908e7DBb3a0f2581aa8F1962f9360e10DC059',
+      ethAddress: '0x531b92991c4EC8Bb18E39E6180aa2350e434C42D',
       spacePublicData: [],
       spacePrivateData: [],
       isAuthenticated: false,
@@ -35,8 +35,8 @@ class App extends React.Component {
   }
 
   async loadPublic() {
-
-    const spaceData = await Box.getSpace(this.state.ethAddress, 'pizza')
+    // If you only need to display public space data for a user or multiple users in your application, you can use the static read-only get methods
+    const spaceData = await Box.getSpace(this.state.ethAddress, 'spacesdemo')                 // spacesdemo is the name used for this Apps 3Box Data Space
 
     this.setState({
       backgroundColor: spaceData.backgroundColor,
@@ -45,28 +45,34 @@ class App extends React.Component {
   }
 
   UpdateEthAddress(event) {
-    this.setState({value: event.target.value});
+    this.setState({ethAddress: event.target.value});
   }
 
   boxSyncComplete(){
+    // When you first authenticate the user's 3Box, all data might not be synced from the network yet.
     console.log('Oh yeah synchd for action');
     this.setState({isAuthenticated: true});
   }
 
   spaceSyncComplete(){
+    // We advise against setting any data before this sync has happened.
     console.log('Space Sync');
     this.setState({isSpaceSync: true});
   }
 
   async Open3Box(){
+    // First we need to get the ethereum provider & user address
     const accounts = await window.ethereum.enable();
     console.log(accounts[0]);
+
+    // Then we initialize a new 3Box session
     const box = await Box.openBox(accounts[0], window.ethereum);
     this.setState({box: box});
-    box.onSyncDone(this.boxSyncComplete);
+    box.onSyncDone(this.boxSyncComplete);                                     // When you first authenticate the user's 3Box, all data might not be synced from the network yet.
   }
 
   async Logout3Box(){
+    // Closes 3Box session
     this.state.box.logout();
     this.setState({
       isAuthenticated: false
@@ -74,14 +80,15 @@ class App extends React.Component {
   }
 
   async openSpace(){
-    const space = await this.state.box.openSpace('pizza', { onSyncDone: this.spaceSyncComplete });
+    // If the user already has a space with this name, your application will gain access to it; if the user does not have the space, this method will automatically create one for them.
+    const space = await this.state.box.openSpace('spacesdemo', { onSyncDone: this.spaceSyncComplete });
     console.log('Space opened.')
     this.setState({space: space});
     this.getSpaceData();
   }
 
   async getSpaceData() {
-
+    // This loads all public and private data from the spacesdemo space opened previously with openSpace()
     const spacePublicData = await this.state.space.public.all();
     const spacePrivateData = await this.state.space.private.all();
 
@@ -94,12 +101,15 @@ class App extends React.Component {
       spacePublicData: spacePublicData,
       spacePrivateData: spacePrivateData,
       backgroundColor: spacePublicData.backgroundColor,
-      fontSize: spacePublicData.fontSize
+      fontSize: spacePublicData.fontSize,
+      text: spacePrivateData.text,
+      fontFamily: spacePrivateData.fontFamily,
     })
 
   }
 
   async setPublic(){
+    // Saves to the Spaces Public location. When saving data to a space, use a key to set a value. To perform these actions, you must first authenticate the space.
     console.log('Setting Public: ' + this.state.publicKey + ' ' + this.state.publicValue);
     await this.state.space.public.set(this.state.publicKey, this.state.publicValue);
     console.log('Data saved = should update gui, etc');
@@ -107,6 +117,7 @@ class App extends React.Component {
   }
 
   async setPrivate(){
+    // Saves to the Spaces Private location. When saving data to a space, use a key to set a value. To perform these actions, you must first authenticate the space.
     console.log('Setting Private: ' + this.state.privateKey + ' ' + this.state.privateValue);
     await this.state.space.private.set(this.state.privateKey, this.state.privateValue);
     console.log('Private Data saved = should update gui, etc');
@@ -177,7 +188,7 @@ class App extends React.Component {
       <div>
         <div style={{backgroundColor: this.state.backgroundColor}}>
           <div style={{fontFamily: this.state.fontFamily}}>
-            <h1>{this.state.heading}</h1>
+            <a href="https://docs.3box.io/" target="_blank"><h1>{this.state.heading}</h1></a>
             <p style={{fontSize: this.state.fontSize}}>{this.state.text}</p>
           </div>
         </div>
